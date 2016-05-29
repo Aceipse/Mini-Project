127c077afb0d710963eaccddd61c2bd650c95d24
Pehj <peterhj@gmail.com>
2016-05-29 17:32:39 +0200

    do not count retranmission rate at part of send as this is already in the retransmission part

--- math/comparingRelay.m
+++ math/comparingRelay.m
@@ -26,12 +26,12 @@ t_send_sys = 1*t_send + 1*t_receive;
 e_send_relay_sys = 2*t_send*p_send + 3*t_receive*p_receive + 1*t_overhear*p_overhear;
 e_send_sys = 1*t_send*p_send + 1*t_receive*p_receive;
 
-%considering a specific interval in seconds with 5 packages pr. second send rate
+%considering a specific interval in seconds with 5 packages pr. second send 
 %START configure 
 interval = 3600;
-p_rate = 5/1;
+rate = 5/1;
 re_relay = 1; %(re-1) is % requested retransmissions from c
-re = 100;
+re = 1;
 %STOP configure
 
 %consider the total node time (num_motes*interval), remove node time used
@@ -39,22 +39,22 @@ re = 100;
 %idle_receive in interval. Then add the energy used for the sending 
 %operation for the system.
 
-num_packages = p_rate * interval;   %all packages in interval
-num_retrans = num_packages*re-num_packages %number of retransmissions wihtout relay
+num_packages = rate * interval;   %all packages in interval
 num_relay_retrans = num_packages*re_relay-num_packages %number of retransmissions with relay
+num_retrans = num_packages*re-num_packages %number of retransmissions wihtout relay
 
 num_motes_relay = 3;
 num_motes = 2;
 
 %RELAY
-t_relay_sys_idle = num_motes_relay*interval - t_send_relay_sys*num_packages*re_relay - t_retrans_relay_sys*num_relay_retrans;
+t_relay_sys_idle = num_motes_relay*interval - t_send_relay_sys*num_packages - t_retrans_relay_sys*num_relay_retrans;
 %assert(t_relay_sys_idle>0)
-e_relay_sys = t_relay_sys_idle*p_receive_idle + e_send_relay_sys*num_packages*re_relay + e_retrans_relay_sys*num_relay_retrans
+e_relay_sys = t_relay_sys_idle*p_receive_idle + e_send_relay_sys*num_packages + e_retrans_relay_sys*num_relay_retrans
 
 %NO RELAY
-t_sys_idle = num_motes*interval - t_send_sys*num_packages*re - t_retrans_sys*num_retrans;
+t_sys_idle = num_motes*interval - t_send_sys*num_packages - t_retrans_sys*num_retrans;
 %assert(t_sys_idle>0)
-e_sys = t_sys_idle*p_receive_idle + e_send_sys*num_packages*re + e_retrans_sys*num_retrans
+e_sys = t_sys_idle*p_receive_idle + e_send_sys*num_packages + e_retrans_sys*num_retrans
 
 %plot
 
